8e2672a786094eee978b0dc517fb92b32eb81a98
LRache <2746324475@qq.com>
2024-03-03 13:36:34 +0800

    2.27

--- Practice/2.27-uadd_ok.cpp
+++ Practice/2.27-uadd_ok.cpp
@@ -5,11 +5,6 @@ bool uadd_ok(unsigned x, unsigned y) {
     return x <= sum;
 }
 
-bool tadd_ok(int x, int y) {
-    int sum = x + y;
-    return !((x < 0 && y < 0 && sum > 0) || (x > 0 && y > 0 && sum < 0));
-}
-
 int main() {
     std::cout << sizeof(unsigned);
 }
