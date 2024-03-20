907c7cc022ddaf3afd1f3a396e7a4535f8d13470
Rache <2746324475@qq.com>
2024-03-20 19:42:08 +0800

    bombLab

--- lab/bomb/read_line.c
+++ lab/bomb/read_line.c
@@ -1,10 +1,27 @@
+#include <stdio.h>
+
 union
 {
     char string[4];
     int integer;
-} number_input_strings;
+} number_input_string;
 
+char input_string[0x800];
+FILE *infile;
 
-int skip(int arg0) { // arg0 in rax?
+char *blank_line() {}
 
+char* skip() {
+    char *v3; // rbx
+    while (1) {
+        int v0 = number_input_string.integer; // rax
+        char *v1 = v0 * 0x50; // rdi
+        v1 += (long long)input_string;
+        char *v2 = fgets(v0, infile, 0x50); // rax
+        v3 = v2; // rbx
+        if (v2 == NULL) break;
+    }
+    return v3;
 }
+
+
