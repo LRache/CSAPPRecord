747df66d7d92d0e5c51906d5a7a3efd6be0c1c68
Freedom <11freedom11@sina.cn>
2024-03-20 17:25:15 +0800

    bombLab

---
+++ lab/bomb/phase_defused.c
@@ -0,0 +1,12 @@
+char num_input_string[4];
+
+void phase_defused() {
+    if (*((int*)num_input_string) == 6) return;
+    int v0; // rsp+16
+    int v1; // rsp+12
+    int v2; // rsp+8
+    int *v3 = &v0; // r8
+    int *v4 = &v1; // rcx
+    int *v5 = &v2; // rdx
+    
+}
---
+++ lab/bomb/read_line.c
@@ -0,0 +1,10 @@
+union
+{
+    char string[4];
+    int integer;
+} number_input_strings;
+
+
+int skip(int arg0) { // arg0 in rax?
+
+}
