0c230dbbc3a3922fa5b2463a6ae0e9abcdd50250
Rache <2746324475@qq.com>
2024-04-25 20:05:19 +0800

    coalesce

--- lab/malloclab/mm.c
+++ lab/malloclab/mm.c
@@ -51,29 +51,70 @@ team_t team = {
 #define META_SIZE DWORD_SIZE
 
 #define GET_WORD(p) (*(unsigned int*)(p))
-#define GET_HEADER(p) (GET_WORD((char*)(p) - WORD_SIZE))
 #define GET_BLOCK_SIZE(p) (GET_WORD(p) & ~0x7)
 #define GET_BLOCK_ALLOCATED(p) (GET_WORD(p) & 0x1)
-#define GET_FOOTER(p) (GET_WORD((char*)(p) + GET_BLOCK_SIZE(p) - DOUBLE_WORD_SIZE))
-#define NEXT_HEADER(p) (GET_BLOCK_SIZE(GET_HEADER(p))+(p)-WORD_SIZE)
-#define IS_END(p) (GET_WORD(p) == 0)
+
+#define GET_HEADER(p) (GET_WORD((void*)(p) - WORD_SIZE))
+#define GET_FOOTER(p) (GET_WORD((void*)(p) + GET_BLOCK_SIZE(p) - DWORD_SIZE))
+
+#define NEXT_HEADER(h) (GET_BLOCK_SIZE(h)+(h)-WORD_SIZE)
+#define NEXT_FOOTER(h) (NEXT_HEADER(h) + GET_BLOCK_SIZE(NEXT_HEADER(h)) - WORD_SIZE)
+#define PREV_FOOTER(h) ((void*)h - WORD_SIZE)
+#define PREV_HEADER(h) (PREV_FOOTER(h) - GET_BLOCK_SIZE(PREV_FOOTER(h)) + WORD_SIZE)
+#define IS_END_HEADER(p) (GET_WORD(p) == 0)
+
+#define GET_PREV_ALLOCATED(p) (GET_BLOCK_ALLOCATED((char*)p - WORD_SIZE))
 
 #define SET_WORD(p, val) (GET_WORD(p)=val)
 
 #define PACK(size, allocated) ((size) | (allocated))
 
-static void *head = NULL;
+static void *listHead = NULL;
+
+static void *coalesce(void *p) {
+    void *header = GET_HEADER(p);
+    int prevAllocated = GET_BLOCK_ALLOCATED(PREV_FOOTER(header));
+    int nextAllocated = (!IS_END_HEADER(NEXT_HEADER(header))) && GET_BLOCK_ALLOCATED(NEXT_HEADER(header));
+    
+    size_t pSize = GET_BLOCK_SIZE(header);
+    if (!prevAllocated && !nextAllocated) {
+        return p;
+    }
+    else if (prevAllocated && !nextAllocated) {
+        size_t prevSize = GET_BLOCK_SIZE(PREV_FOOTER(header));
+        size_t newSize = prevSize + pSize;
+        SET_WORD(PREV_HEADER(header), PACK(newSize, 0));
+        SET_WORD(GET_FOOTER(p), PACK(newSize, 0));
+        return PREV_HEADER(header) + WORD_SIZE;
+    }
+    else if (!prevAllocated && nextAllocated) {
+        size_t nextSize = GET_BLOCK_SIZE(NEXT_HEADER(header));
+        size_t newSize = nextSize + pSize;
+        SET_WORD(header, PACK(newSize, 0));
+        SET_WORD(NEXT_FOOTER(header), PACK(newSize, 0));
+        return p;
+    }
+    else {
+        size_t prevSize = GET_BLOCK_SIZE(PREV_FOOTER(header));
+        size_t nextSize = GET_BLOCK_SIZE(NEXT_HEADER(header));
+        size_t newSize = prevSize + pSize + newSize;
+        SET_WORD(PREV_FOOTER(header), PACK(newSize, 0));
+        SET_WORD(NEXT_FOOTER(header), PACK(newSize, 0));
+        return PREV_HEADER(header) + WORD_SIZE;
+    }
+}
 
 static void *extend_heap(size_t s) {
     size_t size = s % 2 ? (s+1) * WORD_SIZE : s * WORD_SIZE;
-    char *p = mem_sbrk(size);
-    if ((long)p == -1) return NULL;
-
-    SET_WORD(p, PACK(size, 0));
-    SET_WORD(p+size-WORD_SIZE, PACK(size, 0));
-    SET_WORD(p+size, 0);
-
-    return p;
+    void *oldBrk = mem_sbrk(size);
+    if ((long)oldBrk == -1) return NULL;
+    ;
+    SET_WORD(oldBrk-WORD_SIZE, PACK(size, 0));
+    SET_WORD(oldBrk, PACK(size, 0));
+    SET_WORD(oldBrk+size-WORD_SIZE, PACK(size, 0));
+    SET_WORD(oldBrk)
+    
+    return coalesce(oldBrk);
 }
 
 /* 
@@ -90,7 +131,7 @@ int mm_init(void)
     if (extend_heap(CHUNK_SIZE / WORD_SIZE) == NULL) {
         return -1;
     }
-    head = oldBrk + DWORD_SIZE;
+    listHead = oldBrk + WORD_SIZE;
     return 0;
 }
 
@@ -101,21 +142,27 @@ int mm_init(void)
 void *mm_malloc(size_t size)
 {
     int newsize = ALIGN(size + META_SIZE);
-    void *p = NEXT_HEADER(head);
-    while (!IS_END(p))
+    void *p = NEXT_HEADER(listHead);
+    while (!IS_END_HEADER(p))
     {
         if (!GET_BLOCK_ALLOCATED(p) && GET_BLOCK_SIZE(p) <= newsize) {
             break;
         }
         p = NEXT_HEADER(p);
     }
-    if (IS_END(p)) {
-        return extend_heap(newsize);
+    if (IS_END_HEADER(p)) {
+        void *oldBrk = extend_heap(newsize);
+        if (p != NULL) {
+            SET_WORD(oldBrk - WORD_SIZE, PACK(newsize, 1));
+            SET_WORD(oldBrk + newsize - WORD_SIZE * 2, PACK(newsize, 1));
+            p = oldBrk;
+        }
     } else {
         SET_WORD(p, PACK(newsize, 1));
         SET_WORD(p+newsize-WORD_SIZE, PACK(newsize, 1));
-        return p+WORD_SIZE;
+        p = p + WORD_SIZE;
     }
+    return p;
 }
 
 /*
@@ -123,6 +170,9 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    if (ptr == NULL) return;
+    void *header = GET_HEADER(ptr);
+    SET_WORD(header, PACK(GET_BLOCK_SIZE(header), 0));
 }
 
 /*
